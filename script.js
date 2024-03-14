db72e56d9824aeb8ae0a04316f62d2d49dcceb88
LUCAS <0910lmadrid4f@gmail.com>
2024-03-14 17:26:20 +0100

    Lucas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const enunciado = document.createElement("h1")
-const boton = document.getElementById("button")
+const boton = document.createElement("button")
 const contenedor = document.createElement("div")
 const body = document.body;
 body.appendChild(enunciado)
@@ -9,43 +9,43 @@ body.appendChild(contenedor)
 console.log(document.body)
 let contador = 0
 boton.addEventListener("click", () => {
-    api()
+    apiCat()
 })
 
-function api() { fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-.then((response) => {
-    console.log(response)
-    if (!response.ok) {
-        throw new Error('La solicitud no fue exitosa');
-    }
-    return response.json();
-})
-.then((data) => {
-   contenedor.innerHTML=`<img src =${(data[0].url)}>`
-}
-)
-.catch ((error)=> {
-    console.log('Error al obtener los datos', error);
-  });
-}
-
-
-// const apiCat = async () => {
-//     try {
-//       const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
-//       if (!response.ok) {
-//         throw new Error('Ha surgido un error', response.status);
-//       }
-//       const data = await response.json();
-//       console.log(data[0].url); // PROBAR
-//     } catch (error) {
-//       console.log('Error al obtener los datos', error);
+// function api() { fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+// .then((response) => {
+//     console.log(response)
+//     if (!response.ok) {
+//         throw new Error('La solicitud no fue exitosa');
 //     }
-//   };
-//   const template = (url, cat) => {
-//     users.forEach((user) => {
-//      console.log(cat.url);
-//     });
-//   };
+//     return response.json();
+// })
+// .then((data) => {
+//    contenedor.innerHTML=`<img src =${(data[0].url)}>` 
+// }
+// )
+// .catch ((error)=> {
+//     console.log('Error al obtener los datos', error);
+//   });
+// }
 
-//   apiCat.then((data)=>template("img", data))
+
+const apiCat = async () => {
+    try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=10");
+      if (!response.ok) {
+        throw new Error('Ha surgido un error', response.status);
+      }
+      const data = await response.json();
+    for (let i = 0; i < data.length; i++){
+        console.log(data[i].url)
+     contenedor.innerHTML+=`<img class="img" src =${(data[i].url)}>`}; // PROBAR
+    } catch (error) {
+      console.log('Error al obtener los datos', error);
+    }
+  };
+  // const template = (url, cat) => {
+  //   cat.forEach((cat) => {
+  //       contenedor.innerHTML=`<img src =${(data[0].url)}>`;
+  //   });
+  // };
